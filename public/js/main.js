e29c973e6d508437aca6268f8122142507276330
Sudhansu Singh <sudhansutillauri@gmail.com>
2021-11-24 11:13:59 +0530

    Weather

---
+++ public/js/main.js
@@ -0,0 +1,76 @@
+const submitbtn = document.getElementById("submitbtn");
+const city = document.getElementById("citynameinput");
+const citydisplayhere = document.getElementById("citydisplayhere");
+
+const temp = document.getElementById("temp");
+const temp_type_image=document.getElementById("temp_type_image");
+const getinfo = async (event) => {
+    event.preventDefault();
+
+    // alert("hi");
+
+    var cityval = city.value;
+    // alert(cityval);
+
+    if (cityval === "") {
+        alert("Please Enter Valid City");
+        citydisplayhere.innerText = "YOUR CITY NAME HERE";
+    }
+
+    else {
+        try {
+            //alert("here");
+            let url = `http://api.openweathermap.org/data/2.5/weather?q=${cityval}&units=metric&appid=d5ab202e7fa81eb3deb114ccc74aabbd`;
+            console.log(url);
+            const response = await fetch(url).then(console.log("fetched"));
+            const data = await response.json().then(console.log("CONVERTED TO OBJECT"));
+            console.log(data);
+            const arrayData= [data];
+            temp.innerText = arrayData[0].main.temp;
+            //temp_type_image.innerText= arrayData[0].weather[0].main;
+
+            citydisplayhere.innerText=`${arrayData[0].name}, ${arrayData[0].sys.country}`
+            const tempMood=arrayData[0].weather[0].main;
+            console.log(tempMood);
+            if(tempMood=="Clear")
+            {
+                temp_type_image.innerHTML= "<i class='fas fa-sun fa-10x'> </i>";
+            }
+            else if(tempMood=="Clouds")
+            temp_type_image.innerHTML= "<i class='fas fa-cloud fa-10x'></i>";
+            else if(tempMood=="Rain")
+            temp_type_image.innerHTML= "<i class='fas fa-cloud-rain fa-10x'></i>";
+            else
+            temp_type_image.innerHTML= "<i class='fas fa-sun fa-10x'></i>";
+
+            const min=document.getElementById("Min");
+            min.innerText= arrayData[0].main.temp_min;
+            //min.innerText="hello";
+            const max=document.getElementById("Max");
+            max.innerText= arrayData[0].main.temp_max;
+            const feels_like=document.getElementById("feels_like");
+            feels_like.innerText= arrayData[0].main.feels_like;
+            const humidity=document.getElementById("humidity");
+            humidity.innerText= arrayData[0].main.humidity;
+            const visibility=document.getElementById("visibility");
+            visibility.innerText= arrayData[0].visibility/1000;
+
+
+
+        }
+
+        catch 
+        {
+            //alert("Enter correct name");
+            citydisplayhere.innerText = "Enter Correct city name";
+            temp.innerText ='0';
+        }
+
+        // console.log("okaY");
+
+    }
+
+}
+submitbtn.addEventListener("click", getinfo);
+
+
---
+++ src/app.js
@@ -0,0 +1,53 @@
+const express=require("express");
+const app= express();
+const path=require("path");
+const hbs=require("hbs");
+const port=process.env.PORT || 3000;
+
+app.set("view engine", "hbs");
+templates_path=path.join(__dirname, "../templates/views");
+app.set("views", templates_path)
+
+partials_path=path.join(__dirname, "../templates/partials");
+hbs.registerPartials(partials_path);
+
+
+pathname=path.join(__dirname, "../public");
+app.use(express.static(pathname));
+
+
+
+
+app.get("/", (req, res)=>
+{
+     res.render("index");
+})
+app.get("/index", (req, res)=>
+{
+     res.render("index");
+})
+app.get("/about", (req, res)=>
+{
+     res.render("about");
+})
+app.get("/weather", (req, res)=>
+{
+     res.render("Weather");
+})
+app.get("*", (req, res)=>
+{
+console.log("errorpage");
+     res.render("404error.hbs", {
+          errorMsg:"Opps! Page not found",
+     });
+})
+
+
+
+
+
+
+app.listen(port, ()=>
+{
+    console.log(`listening to port ${port}`);
+})
